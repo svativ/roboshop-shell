1820818206a1905f603db60dbf8b699f22aa61b1
svati <svativangapandu9@gmail.com>
2023-08-28 01:52:35 +0530

    Password NFR

--- mysql.sh
+++ mysql.sh
@@ -1,6 +1,6 @@
 mysql_root_password=$1
 if [ -z "${mysql_root_Password}" ]; then
-  echo INputPassword Missing
+  echo INput Password Missing
   exit 1
 fi
 cp mysql.repo /etc/yum.repos.d/mysql.repo
