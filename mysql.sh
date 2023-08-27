d32672a263d8f479801d55e17b714dabcc0e26e4
svati <svativangapandu9@gmail.com>
2023-08-28 01:32:00 +0530

    Password NFR

--- mysql.sh
+++ mysql.sh
@@ -1,6 +1,11 @@
+mysql_root-password=$1
+if [ -z "${mysql_root-password}" ]; then
+  echo INput password Missing
+  exit 1
+fi
 cp mysql.repo /etc/yum.repos.d/mysql.repo
 yum module disable mysql -y
 yum install mysql-community-server -y
 systemctl enable mysqld
 systemctl start mysqld
-mysql_secure_installation --set-root-pass RoboShop@1
+mysql_secure_installation --set-root-pass ${mysql_root-password}
