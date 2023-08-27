5295e0eaf5767e78d21d61261fd50df571f90746
svati <svativangapandu9@gmail.com>
2023-08-27 22:22:45 +0530

    Schema setup in roboshop

--- common.sh
+++ common.sh
@@ -3,44 +3,54 @@ log=/tmp/roboshop.log
 func_apppreq() {
     echo -e "\e[36m>>>>>>>>>>> Create ${component} Service  <<<<<<<<<<<\e[0m"
     cp ${component}.service /etc/systemd/system/${component}.service &>>${log}
+    echo $?
 
     echo -e "\e[36m>>>>>>>>>>> Create Application User <<<<<<<<<<<\e[0m"
     useradd roboshop &>>${log}
+    echo $?
 
     echo -e "\e[36m>>>>>>>>>>> Cleanup Existing Application Content <<<<<<<<<<<\e[0m"
     rm -rf /app &>>${log}
+    echo $?
 
     echo -e "\e[36m>>>>>>>>>>> Create Application Direcctory <<<<<<<<<<<\e[0m"
     mkdir /app &>>${log}
-
+    echo $?
     echo -e "\e[36m>>>>>>>>>>> Download application content <<<<<<<<<<<\e[0m"
     curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log}
-
+    echo $?
     echo -e "\e[36m>>>>>>>>>> Extract Application Content <<<<<<<<<<<\e[0m"
     cd /app
     unzip /tmp/${component}.zip &>>${log}
-    cd /app
+    echo $?
+
 }
  func_systemd() {
   echo -e "\e[36m>>>>>>>>>>> Start ${component} Service  <<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
   systemctl daemon-reload &>>${log}
   systemctl enable ${component} &>>${log}
   systemctl restart ${component} &>>${log}
+  echo $?
 }
 func_schema_setup() {
   if [ "${schema_type}" == "mongodb" ]; then
     echo -e "\e[36m>>>>>>>>>>> Install Mongo Client <<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
     yum install mongodb-org-shell -y &>>${log}
+    echo $?
 
     echo -e "\e[36m>>>>>>>>>>> Load user Schema <<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
     mongo --host mongodb.sdevops99.online </app/schema/${component}.js &>>${log}
+    echo $?
   fi
 
- if  [ "${schema_type}" == "mysql" ]; then
+ if [ "${schema_type}" == "mysql" ]; then
    echo -e "\e[36m>>>>>>>>>>> Install MySQL Client   <<<<<<<<<<<\e[0m"
    yum install mysql -y &>>${log}
+   echo $?
+
    echo -e "\e[36m>>>>>>>>>>> Load Schema   <<<<<<<<<<<\e[0m"
    mysql -h mysql.sdevops99.online -uroot -pRoboShop@1 < /app/schema/${component}.sql &>>${log}
+   echo $?
  fi
 }
 func_nodejs() {
@@ -48,19 +58,24 @@ func_nodejs() {
 
   echo -e "\e[36m>>>>>>>>>>> Create MongoDB Repo <<<<<<<<<<<\e[0m"
   cp mongo.repo /etc/yum.repos.d/mongo.repo &>>${log}
+  echo $?
 
   echo -e "\e[36m>>>>>>>>>>> Install NodeJS Repo <<<<<<<<<<<\e[0m"
   curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log}
+  echo $?
 
   echo -e "\e[36m>>>>>>>>>>> Install NodeJS <<<<<<<<<\e[0m"
   yum install nodejs -y &>>${log}
+  echo $?
 
   func_apppreq
 
   echo -e "\e[36m>>>>>>>>>>> Download Nodejs Client <<<<<<<<<<<\e[0m"
   npm install &>>${log}
+  echo $?
 
-func_schema_setup
+  func_schema_setup
+
   func_systemd
  }
 
