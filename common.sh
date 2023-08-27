ec05b7408d10266529f337426f70d957a8cca86e
svati <svativangapandu9@gmail.com>
2023-08-27 23:12:42 +0530

    Schema setup in roboshop

--- common.sh
+++ common.sh
@@ -1,9 +1,9 @@
 log=/tmp/roboshop.log
 
-  func__exit_status() {
+func__exit_status() {
    if [ $? -eq 0 ]; then
       echo -e "\e[32m SUCCESS \e[0m"
-      else
+     else
       echo -e "\e[31m FAILURE \e[0m"
     fi
 }
