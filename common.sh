f06fecc49a1cce7192e13e558325bfd3f5c1b4de
svati <svativangapandu9@gmail.com>
2023-08-28 15:54:49 +0530

    catalogue NFRS

--- common.sh
+++ common.sh
@@ -45,8 +45,8 @@ func_apppreq() {
 }
 func_schema_setup() {
   if [ "${schema_type}" == "mongodb" ]; then
-    echo -e "\e[36m>>>>>>>>>>> Install Mongo Client <<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
-    yum install mongodb-org-shell -y &>>${log}
+      echo -e "\e[36m>>>>>>>>>>>>  INstall Mongo Client  <<<<<<<<<<<<\e[0m"  | tee -a /tmp/roboshop.log
+      yum install mongodb-org-shell -y &>>${log}
     func_exit_status
 
     echo -e "\e[36m>>>>>>>>>>>>  Load User Schema  <<<<<<<<<<<<\e[0m"  | tee -a /tmp/roboshop.log
