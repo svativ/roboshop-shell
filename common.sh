9a83a0ae34c4308de07045766f5eb8aa802c15a0
svati <svativangapandu9@gmail.com>
2023-08-28 17:37:39 +0530

    catalogue NFRS

--- common.sh
+++ common.sh
@@ -9,65 +9,66 @@ func_exit_status() {
 }
 
 func_apppreq() {
-    echo -e "\e[36m>>>>>>>>>>> Create ${component} service <<<<<<<<<<<\e[0m"
-    cp  ${component}.service /etc/systemd/system/${component}.service &>>${log}
-    func_exit_status
+  echo -e "\e[36m>>>>>>>>>>>>  Create ${component} Service  <<<<<<<<<<<<\e[0m"
+  cp ${component}.service /etc/systemd/system/${component}.service &>>${log}
+  func_exit_status
 
-    echo -e "\e[36m>>>>>>>>>>> Create Application User <<<<<<<<<<<\e[0m"
-    id roboshop &>>${log}
-    if [ $? -ne 0 ]; then
-      useradd roboshop &>>${log}
-    fi
-    func_exit_status
-    echo -e "\e[36m>>>>>>>>>>> Cleanup Existing Application Content <<<<<<<<<<<\e[0m"
-    rm -rf /app &>>${log}
-    func_exit_status
+  echo -e "\e[36m>>>>>>>>>>>>  Create Application User  <<<<<<<<<<<<\e[0m"
+  id roboshop &>>${log}
+  if [ $? -ne 0 ]; then
+    useradd roboshop &>>${log}
+  fi
+  func_exit_status
 
-    echo -e "\e[36m>>>>>>>>>>> Create Application Direcctory <<<<<<<<<<<\e[0m"
-    mkdir /app &>>${log}
-    func_exit_status
-    echo -e "\e[36m>>>>>>>>>>> Download application content <<<<<<<<<<<\e[0m"
-    curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zipc &>>${log}
-    func_exit_status
+  echo -e "\e[36m>>>>>>>>>>>>  Cleanup Existing Application Content  <<<<<<<<<<<<\e[0m"
+  rm -rf /app &>>${log}
+  func_exit_status
 
-    echo -e "\e[36m>>>>>>>>>>>>  Extract Application Content  <<<<<<<<<<<<\e[0m"
-     cd /app
-     unzip /tmp/${component}.zip &>>${log}
-     func_exit_status
+  echo -e "\e[36m>>>>>>>>>>>>  Create Application Directory  <<<<<<<<<<<<\e[0m"
+  mkdir /app &>>${log}
+  func_exit_status
+
+  echo -e "\e[36m>>>>>>>>>>>>  Download Application Content  <<<<<<<<<<<<\e[0m"
+  curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log}
+  func_exit_status
+
+  echo -e "\e[36m>>>>>>>>>>>>  Extract Application Content  <<<<<<<<<<<<\e[0m"
+  cd /app
+  unzip /tmp/${component}.zip &>>${log}
+  func_exit_status
 }
- func_systemd() {
-  echo -e "\e[36m>>>>>>>>>>> Start ${component} Service  <<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
+
+func_systemd() {
+  echo -e "\e[36m>>>>>>>>>>>>  Start ${component} Service  <<<<<<<<<<<<\e[0m"  | tee -a /tmp/roboshop.log
   systemctl daemon-reload &>>${log}
   systemctl enable ${component} &>>${log}
   systemctl restart ${component} &>>${log}
-
   func_exit_status
 }
+
 func_schema_setup() {
-
   if [ "${schema_type}" == "mongodb" ]; then
-        echo -e "\e[36m>>>>>>>>>>>>  INstall Mongo Client  <<<<<<<<<<<<\e[0m"  | tee -a /tmp/roboshop.log
-        yum install mongodb-org-shell -y &>>${log}
-        func_exit_status
+    echo -e "\e[36m>>>>>>>>>>>>  INstall Mongo Client  <<<<<<<<<<<<\e[0m"  | tee -a /tmp/roboshop.log
+    yum install mongodb-org-shell -y &>>${log}
+    func_exit_status
 
-        echo -e "\e[36m>>>>>>>>>>>>  Load User Schema  <<<<<<<<<<<<\e[0m"  | tee -a /tmp/roboshop.log
-        mongo --host mongodb.sdevop99.online </app/schema/${component}.js &>>${log}
-        func_exit_status
+    echo -e "\e[36m>>>>>>>>>>>>  Load User Schema  <<<<<<<<<<<<\e[0m"  | tee -a /tmp/roboshop.log
+    mongo --host mongodb.rdevopsb72.online </app/schema/${component}.js &>>${log}
+    func_exit_status
   fi
 
-
-
   if [ "${schema_type}" == "mysql" ]; then
-   echo -e "\e[36m>>>>>>>>>>> Install MySQL Client   <<<<<<<<<<<\e[0m"
-   yum install mysql -y &>>${log}
-   func_exit_status
+    echo -e "\e[36m>>>>>>>>>>>>  Install MySQL Client   <<<<<<<<<<<<\e[0m"
+    yum install mysql -y &>>${log}
+    func_exit_status
 
-   echo -e "\e[36m>>>>>>>>>>> Load Schema   <<<<<<<<<<<\e[0m"
-   mysql -h mysql.sdevops99.online -uroot -pRoboShop@1 < /app/schema/${component}.sql &>>${log}
-   func_exit_status
- fi
+    echo -e "\e[36m>>>>>>>>>>>>  Load Schema   <<<<<<<<<<<<\e[0m"
+    mysql -h mysql.rdevopsb72.online -uroot -pRoboShop@1 < /app/schema/${component}.sql &>>${log}
+    func_exit_status
+  fi
 
 }
+
 
 func_nodejs() {
   log=/tmp/roboshop.log
